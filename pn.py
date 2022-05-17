b172e8e078cd2495377fd818bbbbe91cc33c5b68
alfaPegasis <anmolbajpai2001@gmail.com>
2022-05-17 23:17:37 +0530

    arbitrary codes push in

---
+++ pn.py
@@ -0,0 +1,26 @@
+def nqueens(n):
+  col=set()
+  pdiag=set()
+  ndiag=set()
+  res=[]
+  board=[["."]* n for i in range(n)]
+  def backtrack(r):
+    if r==n:
+      copy=["".join(row)for row in board]
+      res.append(copy)
+      return
+    for c in range(n):
+      if c in col or (r+c) in pdiag or (r-c) in ndiag:
+        continue
+      col.add(c)
+      pdiag.add(r+c)
+      ndiag.add(r-c)
+      board[r][c]="q"
+      backtrack(r+1)
+      col.remove(c)
+      pdiag.remove(r+c)
+      ndiag.remove(r-c)
+      board[r][c]="."
+  backtrack(0)
+  return res
+print(nqueens(8))
